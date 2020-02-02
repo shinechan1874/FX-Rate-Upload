1bd50701e4a65d283eee5657b04c655194aca313
shine chen <59347996+shinechan1874@users.noreply.github.com>
2020-02-02 14:49:03 +1100

    Monthly FX Rates Upload

--- Monthly FX Rates Upload.py
+++ Monthly FX Rates Upload.py
@@ -11,6 +11,11 @@ import pandas as pd
 from selenium.webdriver.support.wait import WebDriverWait
 import calendar
 
+downloadlink = "***"
+uploadlink = "***"
+username = "***"
+password = "***"
+
 month_number_map = {"Jan":1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
 
 upload_month_1 = "Jan"
@@ -26,13 +31,13 @@ driver_options.add_experimental_option("prefs", {'download.default_directory':do
                                                  'download.directory_upgrade':True, "plugins.always_open_pdf_externally":True})
 driver = webdriver.Chrome("Driver/chromedriver.exe", options=driver_options)
 
-driver.get("textwebsite")
+driver.get(downloadlink)
 time.sleep(3)
 download = driver.find_element_by_link_text("Month End FX Rates")
 download.click()
 time.sleep(2)
 
-month_end_FX_Rates = pd.ExcelFile("filedirectory")
+month_end_FX_Rates = pd.ExcelFile("C:/Users/SXC57/PycharmProjects/Monthly Fx Uplaod/Month End FX Rates.xls")
 fx_rates = month_end_FX_Rates.parse("Report")
 fx_rates = fx_rates.iloc[:, 0:3]
 #for current month rate
@@ -52,10 +57,10 @@ fx_rates.iloc[3,0] = f"The following rates are to be applied for accounting purp
 fx_rates.to_csv(uploadfilename2, index=False)
 
 driver = webdriver.Chrome("Driver/chromedriver.exe")
-driver.get("textweblink")
+driver.get(uploadlink)
 time.sleep(5)
-driver.find_element_by_name("username").send_keys("username")
-driver.find_element_by_name("password").send_keys("userpassword")
+driver.find_element_by_name("username").send_keys(username)
+driver.find_element_by_name("password").send_keys(password)
 driver.find_element_by_id("btnSiteLogin").click()
 
 # uploadbutton = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, "Selectedfile")))
